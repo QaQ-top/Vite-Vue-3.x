84e008e866e0929ac2b728ca029828188b9994c2
QaQ-top <294198354@qq.com>
2020-08-29 06:56:42 +0800

    添加样式

---
+++ src/components/api/inject/components/inject.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>子组件： <code>inject</code>: 获取依赖 </p>
+    <p>{{response.value}}</p>
+  </div>
+  <pre-code :code='response' />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Injectchildren',
+  setup: () => {
+    return {
+    }
+  },
+  //>—— inject ——
+  /**
+   * 获取 祖先 元素的依赖注入
+   */
+  inject: {
+    receive: 'passed',
+    response: 'response'
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/api/inject/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>provide</p>
+    <span>依赖注入</span>
+  </div>
+  <div>
+    <childern />
+  </div>
+  <div>
+    <span>Provide</span>
+    <ol type="1">
+      <li>允许一个祖先组件作为其所有后代的依赖注入器</li>
+      <li>无论组件层次结构有多深</li>
+      <li>只要它们位于同一父链中就可以</li>
+    </ol>
+    <span>Inject</span>
+    <ol type="1">
+      <li>获取 祖先 的依赖注入</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import childern from "./components/inject.vue";
+export default {
+  name: "Inject",
+  setup() {
+    return {};
+  },
+  components: {
+    childern,
+  },
+};
+</script>
+
+<style>
+</style>
